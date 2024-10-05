bf5683bf3d291bbcf60c178d68b8937dec1233cc
EONCreator <ITCyborg2001@gmail.com>
2024-10-05 07:00:53 +0300

    user-service added

---
+++ user-service/src/main/java/com/example/userservice/domain/entities/User.java
@@ -0,0 +1,11 @@
+package com.example.userservice.domain.entities;
+
+public class User {
+    public int Id;
+    public String UserName;
+    public String PasswordHash;
+    public String UserEmail;
+    public String Phone;
+    public String FirstName;
+    public String LastName;
+}
